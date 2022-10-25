8f239cfc0d62bc5a3eeaba53a10e3205a59cbea2
Kelvin Fong <39546456+NubKel@users.noreply.github.com>
2022-10-26 09:54:17 +1100

    Update job_dsl.groovy
    update url

--- 04/demos/demo2/2.3/job_dsl.groovy
+++ 04/demos/demo2/2.3/job_dsl.groovy
@@ -11,7 +11,7 @@ for(p in pipelines) {
 	                git {
 	                  remote {
 	                    name('github')
-	                    url('https://github.com/sixeyed/jenkins-pipeline-demos.git')
+	                    url('https://github.com/NubKel/Jenkins_prac.git')
 	                  }
 	                  branch('master')
 	                  extensions {
